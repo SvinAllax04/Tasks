276d16a89e82f7fb2b0e6bdc075719071b66b6a4
DirDirkin <imy4familiya.imya@yandex.ru>
2022-12-11 20:54:38 +0300

    Task3_30

---
+++ task3_30/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ru.vsu.cs.tonkikh_r_a.task3_30 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ru.vsu.cs.tonkikh_r_a.task3_30 to javafx.fxml;
+    exports ru.vsu.cs.tonkikh_r_a.task3_30;
+}
